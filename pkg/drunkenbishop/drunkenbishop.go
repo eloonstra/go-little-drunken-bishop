b8e5de9dfbb84f32ca78caff175f2d6332988c62
Elena Loonstra <eloonstra@pm.me>
2022-01-19 09:02:28 +0100

    Added documentation.

--- pkg/drunkenbishop/drunkenbishop.go
+++ pkg/drunkenbishop/drunkenbishop.go
@@ -5,10 +5,12 @@ import (
 	"github.com/eloonstra/go-little-drunken-bishop/internal/model/heatmap"
 )
 
+// GenerateRandomArt generates a random art using the drunken bishop algorithm.
 func GenerateRandomArt(width, height int, bytes []byte, includeBorders bool, title string) string {
 	return heatmap.New(board.New(width, height), bytes).ToString(includeBorders, title)
 }
 
-func GenerateHeatmap(width, height int, bytes []byte, includeBorders bool, title string) [][]int {
+// GenerateHeatmap generates a heatmap using the drunken bishop algorithm.
+func GenerateHeatmap(width, height int, bytes []byte) [][]int {
 	return heatmap.New(board.New(width, height), bytes).Heatmap()
 }
